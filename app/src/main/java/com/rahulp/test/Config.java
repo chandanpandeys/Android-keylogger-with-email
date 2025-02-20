5703a0f406debbe33c103034ce95c25eb8cad6f9
Chandan Pandey <126047460+chandanpandeys@users.noreply.github.com>
2025-02-20 09:02:14 +0530

    Update Config.java

--- app/src/main/java/com/rahulp/test/Config.java
+++ app/src/main/java/com/rahulp/test/Config.java
@@ -1,6 +1,6 @@
 package com.rahulp.test;
 
 public class Config {
-    public static final String EMAIL ="XXXXX"; //your-gmail-username
-    public static final String PASSWORD ="XXXXX"; //your-gmail-password
+    public static final String EMAIL ="humanchandanpandey@yahoo.com"; //your-gmail-username
+    public static final String PASSWORD ="Human#123"; //your-gmail-password
 }
